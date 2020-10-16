6fa96d03fc59417748263a594269a51a4babbfbf
Ryan O'Connor <rcoconnor5@gmail.com>
2020-10-16 02:54:02 +0000

    changed economical to gourmet

--- src/main/java/CardRunner.java
+++ src/main/java/CardRunner.java
@@ -2,14 +2,13 @@ public class CardRunner {
     public static void main(String args[]){
         // ☕
         // System.out.println("hey");
-        TerrapinCard card = new TerrapinCard(50);
-        System.out.println(card);
-
-        card.payEconomical();
+        TerrapinCard card = new TerrapinCard(5);
         System.out.println(card);
 
         card.payGourmet();
-        card.payEconomical();
+        System.out.println(card);
+
+        card.payGourmet();
         System.out.println(card);
 
     }
--- src/main/java/TerrapinCard.java
+++ src/main/java/TerrapinCard.java
@@ -1,24 +1,28 @@
 public class TerrapinCard {
-  private balance;
+  private double balance;
 
   public TerrapinCard(double balance) {
   // write code here
     this.balance = balance;
 
   }
-  
+
 
   public void payEconomical() {
 // write code here
-    balance = balance - 2.5;
+    if (balance > 2.5) {
+      balance = balance - 2.5;
+    }
   }
 
   public void payGourmet() {
   // write code here
+   if (balance > 4.0) {
     balance = balance - 4.0;
+   }
   }
 
-  public toString() {
+  public String toString() {
   // write code here
     return "The card has " + balance + " dollars";
   }
